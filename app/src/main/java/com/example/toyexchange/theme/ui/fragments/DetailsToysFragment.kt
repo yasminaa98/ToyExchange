e4f47467616c5df19ea31b141bdf13b28c0b4707
yasminaa98 <yasmina.bns@hotmail.fr>
2023-05-03 00:57:39 +0200

    Owner

--- app/src/main/java/com/example/toyexchange/theme/ui/fragments/DetailsToysFragment.kt
+++ app/src/main/java/com/example/toyexchange/theme/ui/fragments/DetailsToysFragment.kt
@@ -1,16 +1,21 @@
 package com.example.toyexchange.theme.ui.fragments
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.toyexchange.Domain.model.Toy
 import com.example.toyexchange.Presentation.ToysViewModel.DetailsToyViewModel
-import com.example.toyexchange.Presentation.ToysViewModel.DetailsToyViewModelFactory
+import com.example.toyexchange.Presentation.ToysViewModel.RoomViewModel
+import com.example.toyexchange.Presentation.ToysViewModel.RoomViewModelFactory
 import com.example.toyexchange.R
 import com.example.toyexchange.databinding.ToyDetailsFragmentBinding
 import com.example.toyexchange.db.ToyDatabase
@@ -20,7 +25,10 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DetailsToysFragment : Fragment(R.layout.toy_item) {
     private lateinit var detailsToyViewModel: DetailsToyViewModel
+    private lateinit var roomViewModel: RoomViewModel
 
+
+    @SuppressLint("ResourceAsColor", "ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,15 +36,74 @@ class DetailsToysFragment : Fragment(R.layout.toy_item) {
     ): View {
         val binding = ToyDetailsFragmentBinding.inflate(inflater, container, false)
         val toyDatabase = ToyDatabase.getInstance(requireContext())
+        val roomViewModelFactory = RoomViewModelFactory(toyDatabase)
+        roomViewModel =
+            ViewModelProvider(this, roomViewModelFactory)[RoomViewModel::class.java] //onFavoriteClick()
+        detailsToyViewModel = ViewModelProvider(this).get(DetailsToyViewModel::class.java)
 
-        val detailsViewModelFactory = DetailsToyViewModelFactory(toyDatabase)
-        detailsToyViewModel =
-            ViewModelProvider(this, detailsViewModelFactory)[DetailsToyViewModel::class.java] //onFavoriteClick()
-        //maintain checkbox state :
-        val toyId = arguments?.getInt("id")
+        //get current user
         val sharedPreferences =
+            requireActivity().getSharedPreferences("authToken", Context.MODE_PRIVATE)
+        val token = sharedPreferences.getString("authToken", null)
+        val username = sharedPreferences.getString("username", null)
+        val toyId = arguments?.getLong("id")
+
+        // getAnnonceOwner
+        detailsToyViewModel.getAnnonceOwner(toyId!!)
+
+
+
+        binding.ownerImage.setOnClickListener {
+            if (binding.profileOwner.visibility==View.GONE) {
+                Log.i("button clicked", "")
+                binding.profileOwner.apply {
+                    visibility = View.VISIBLE
+                    translationY = height.toFloat()
+                    // Animate from off-screen to original position
+                    animate().apply {
+                        translationY(-1400f)
+                    }
+                }
+            }
+        }
+        binding.toyDetailsFragment.setOnClickListener {
+            if (binding.profileOwner.visibility == View.VISIBLE) {
+                Log.i("button clicked", "")
+                binding.profileOwner.apply {
+                    visibility = View.GONE
+                    translationY = height.toFloat()
+                    // Animate from off-screen to original position
+                    animate().apply {
+                        translationY(1400f)
+                    }
+                }
+            }
+        }
+        detailsToyViewModel.annonceOwner.observe(viewLifecycleOwner, Observer {
+            if (it!=null){
+                if (it.username==username){
+                    binding.exchange.setBackgroundColor(R.color.grey)
+                    binding.exchange.isEnabled=true
+                    binding.exchange.setText("Exchange unavailable")
+                }
+                binding.apply {
+                    userName.setText(it.username)
+                    firstname.setText(it.firstname)
+                    lastname.setText(it.lastname)
+                    email.setText(it.email)
+                    homeaddress.setText(it.homeAddress)
+                    phone.setText(it.phone.toString())
+                    avgResponse.setText(it.avgResponseTime)
+
+                }
+
+
+            }
+        })
+        //maintain checkbox state :
+        val sharedPreferencesRoom =
             requireActivity().getSharedPreferences("my_checkbox_${toyId}", Context.MODE_PRIVATE)
-        val checkboxState = sharedPreferences.getBoolean("checkbox_state", false)
+        val checkboxState = sharedPreferencesRoom.getBoolean("checkbox_state", false)
         binding.checkBox.isChecked = checkboxState
 
         // get item selected
@@ -54,15 +121,16 @@ class DetailsToysFragment : Fragment(R.layout.toy_item) {
         }
         val toyToSave = Toy(toyId!!, category, description, image, name, price,"","","")
         binding.checkBox.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("checkbox_state", isChecked).apply()
+            sharedPreferencesRoom.edit().putBoolean("checkbox_state", isChecked).apply()
             if (isChecked){
-            detailsToyViewModel.insertToy(toyToSave)
+            roomViewModel.insertToy(toyToSave)
             Toast.makeText(requireContext(), "Toy saved", Toast.LENGTH_SHORT).show()}
             else{
-                detailsToyViewModel.deleteToy(toyToSave)
+                roomViewModel.deleteToy(toyToSave)
                 Toast.makeText(requireContext(), "Toy deleted", Toast.LENGTH_SHORT).show()}
 
         }
+
         return binding.root
     }
 }
